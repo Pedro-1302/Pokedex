04078e6a16f5e81cc10c30fce2757012db3d95af
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-05-31 13:56:10 -0300

    create project

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Pedro Franco on 31/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
