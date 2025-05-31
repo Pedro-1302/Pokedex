ca31184e633be06a4357b59a49fecaf1ac269545
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-05-31 15:59:24 -0300

    create PokemonCellView

---
+++ Pokedex/Pokedex/Shared/Components/Views/PokemonCellView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonCellView.swift
+//  Pokedex
+//
+//  Created by Pedro Franco on 31/05/25.
+//
+
+import UIKit
+
+class PokemonCellView: UIView {
+
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+
+}
