56771335e39689429d5b69716a1bae0975f5b4b6
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-05-31 14:49:33 -0300

    add SwiftLint

--- Pokedex/Pokedex/SceneDelegate.swift
+++ Pokedex/Pokedex/SceneDelegate.swift
@@ -11,7 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = scene as? UIWindowScene else { return }
         let viewController = ViewController()
         let navController = UINavigationController(rootViewController: viewController)
@@ -31,4 +35,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) { }
 }
-
