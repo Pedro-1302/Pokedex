6ec0a37320906aa154f65c7919fae3d99d60968d
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-06-09 20:18:00 -0300

    create PokemonDetail

---
+++ Pokedex/Pokedex/Models/PokemonDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetail.swift
+//  Pokedex
+//
+//  Created by Pedro Franco on 09/06/25.
+//
+
+import Foundation
+
+struct PokemonDetail: Codable {
+
+}
+
+extension PokemonDetail {
+    static func createMock() -> PokemonDetail {
+        return PokemonDetail()
+    }
+}
