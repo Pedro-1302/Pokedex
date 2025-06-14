812b5653aa250fdc2e7ca7326dd3ae660beb4678
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-06-13 23:07:54 -0300

    refactor PokemonTypeSlot

--- Pokedex/Pokedex/Models/PokemonDetail/PokemonTypeSlot.swift
+++ Pokedex/Pokedex/Models/PokemonDetail/PokemonTypeSlot.swift
@@ -1,10 +1,3 @@
-//
-//  PokemonTypeSlot.swift
-//  Pokedex
-//
-//  Created by Pedro Franco on 09/06/25.
-//
-
 import Foundation
 
 struct PokemonTypeSlot: Codable {
@@ -14,6 +7,7 @@ struct PokemonTypeSlot: Codable {
 
 extension PokemonTypeSlot {
     static func createMock() -> PokemonTypeSlot {
-        PokemonTypeSlot(slot: 1, type: .createMock())
+        PokemonTypeSlot(slot: 1,
+                        type: .createMock())
     }
 }
