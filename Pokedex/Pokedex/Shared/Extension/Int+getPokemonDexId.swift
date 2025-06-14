f46db0913661c1bdfcca58a4c78bf602f1a1da1b
Pedro Franco <pedrofranco@Pedros-MacBook-Pro.local>
2025-06-13 22:57:54 -0300

    refactor Int+getPokemonDexId

--- Pokedex/Pokedex/Shared/Extension/Int+getPokemonDexId.swift
+++ Pokedex/Pokedex/Shared/Extension/Int+getPokemonDexId.swift
@@ -1,14 +1,7 @@
-//
-//  Int+getPokemonDexId.swift
-//  Pokedex
-//
-//  Created by Pedro Franco on 31/05/25.
-//
-
 import Foundation
 
 extension Int {
     func getPokemonDexId() -> String {
-        return String(format: "#%03d", self)
+        String(format: "#%03d", self)
     }
 }
